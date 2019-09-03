03a105b5729a8266acb0b59e3c72b7d1f16ce26f
Markus Göckeritz <markus@goeckeritz.ch>
2019-09-03 21:32:00 +0200

    added script to set up tools in venv

--- scripts/config.sh
+++ scripts/config.sh
@@ -3,5 +3,6 @@ APP_NAME="YouMayNotNeedAttention"
 SCRIPTS_DIR=`dirname "$0"`
 BASE_DIR=${SCRIPTS_DIR}/..
 VENVS_DIR=${BASE_DIR}/venvs/
+TOOLS_DIR=${BASE_DIR}/tools/
 
-PYTHON_VERSION="python3.7"
+PYTHON_VERSION="python3.6"
---
+++ scripts/download_install_packages.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+SCRIPTS_DIR=`dirname "$0"`
+BASE_DIR=${SCRIPTS_DIR}/..
+
+source ${SCRIPTS_DIR}/config.sh
+
+python_location=`which python`
+
+if [[ $python_location == *"/usr/bin"* ]]; then
+    echo "Do not run this script without activating a virtual environment first"
+    echo "To activate your virtual environment:"
+    echo "source ${VENVS_DIR}/${APP_NAME}/bin/activate"
+    exit
+fi
+
+mkdir -p ${TOOLS_DIR}
+
+# install sockeye for sockeye-autopilot
+pip install sockeye --no-deps
+
+# install sacreBLEU for evaluation
+pip install sacrebleu
+
+# install Moses scripts for preprocessing
+git clone https://github.com/bricksdont/moses-scripts ${TOOLS_DIR}/moses-scripts
+
+git clone https://github.com/clab/fast_align.git ${TOOLS_DIR}/fast_align
+cd ${TOOLS_DIR}/fast_align
+cmake .
+make
