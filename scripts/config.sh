05cf80276039c7437b1f08809be4532e36b2ebc9
Markus Göckeritz <markus@goeckeritz.ch>
2019-09-03 21:36:22 +0200

    removed slash in dirs

--- scripts/config.sh
+++ scripts/config.sh
@@ -2,7 +2,7 @@ APP_NAME="YouMayNotNeedAttention"
 
 SCRIPTS_DIR=`dirname "$0"`
 BASE_DIR=${SCRIPTS_DIR}/..
-VENVS_DIR=${BASE_DIR}/venvs/
-TOOLS_DIR=${BASE_DIR}/tools/
+VENVS_DIR=${BASE_DIR}/venvs
+TOOLS_DIR=${BASE_DIR}/tools
 
 PYTHON_VERSION="python3.6"
