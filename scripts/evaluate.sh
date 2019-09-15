083d685da1a61e772d0bdcad6d938056bc86cf0f
Markus Göckeritz <markus@goeckeritz.ch>
2019-09-15 18:21:50 +0200

    added linebreaks to script

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -7,14 +7,14 @@ CHECKPOINT="model357500.pt"
 DATA="corpus/WMTENDE/4pad"
 
 python model/generate.py \
-    --checkpoint exps/model357500.pt
-    --data corpus/WMTENDE/4pad
-    --src_path sockeye_autopilot/systems/wmt14_en_de/data/bpe/dev.src
-    --beam_size 5
-    --eval
-    --target_translation sockeye_autopilot/systems/wmt14_en_de/data/tst/dev.trg
-    --epsilon_limit 3
-    --src_epsilon_injection 4
-    --start_pads 5
-    --language de
+    --checkpoint exps/model390000.pt \
+    --data corpus/WMTENDE/4pad \
+    --src_path sockeye_autopilot/systems/wmt14_en_de/data/bpe/dev.src \
+    --beam_size 5 \
+    --eval \
+    --target_translation sockeye_autopilot/systems/wmt14_en_de/data/tst/dev.trg \
+    --epsilon_limit 3 \
+    --src_epsilon_injection 4 \
+    --start_pads 5 \
+    --language de \
     --save_dir output/
