005861c1675f517fc28893854970c29df72b9861
Markus Göckeritz <markus@goeckeritz.ch>
2019-09-21 17:26:32 +0200

    added preprocess, fixed wrong dir in download

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -5,6 +5,6 @@ source ${SCRIPTS_DIR}/config.sh
 
 sockeye-autopilot --task ${LANGUAGE_DATA} --model none --workspace ${DATA_DIR}
 
-cd ./${DATA_DIR}/systems/${LANGUAGE_DATA}/data/bpe
+cd ${DATA_DIR}/systems/${LANGUAGE_DATA}/data/bpe
 gunzip *
 paste -d '|'  train.src train.trg | shuf | awk -v FS='|' '{ print $1 > "train.shuf.src" ; print $2 > "train.shuf.trg" }'
---
+++ scripts/prepocess.sh
@@ -0,0 +1,25 @@
+SCRIPTS_DIR=`dirname "$0"`
+BASE_DIR=${SCRIPTS_DIR}/..
+
+source ${SCRIPTS_DIR}/config.sh
+
+# Combine the source and target training data into one file
+paste -d ' ||| ' train.shuf.src - - - - train.shuf.trg < /dev/null > combined_srctrg
+
+# There are sentence pairs where either side is empty
+# This will lead to fast align not being able to work propery, hence, remove these lines:
+cp combined_srctrg combined_srctrg_clean
+sed -i '/^ |||/d' combined_srctrg_clean
+sed -i '/ ||| $/d' combined_srctrg_clean
+
+
+# Use fast_align to find the aligments of the training sentence pairs
+alias fast_align=${TOOLS_DIR}/fast_align/fast_align
+fast_align -i ${DATA_DIR}/systems/${LANGUAGE_DATA}/data/bpe/combined_srctrg_clean -d -o -v > forward.align_ende
+
+# Run our script for making the training data Eager Feasible:
+# Because add_epsilon performs in-place operations, we first create a copy
+cp forward.align_ende forward.align_ende.bkp
+python add_epsilons.py --align forward.align_ende --trg ${DATA_DIR}/systems/${LANGUAGE_DATA}/data/bpe/
+
+
