293343130e5270efa57f90ed2d7974d0af7da5a5
Markus Göckeritz <markus@goeckeritz.ch>
2019-09-21 17:19:20 +0200

    changed data dir back to sockeye

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -3,10 +3,8 @@ BASE_DIR=${SCRIPTS_DIR}/..
 
 source ${SCRIPTS_DIR}/config.sh
 
-SOCKEYE_DATA_DIR=${DATA_DIR}/sockeye_autopilot
+sockeye-autopilot --task ${LANGUAGE_DATA} --model none --workspace ${DATA_DIR}
 
-sockeye-autopilot --task ${LANGUAGE_DATA} --model none --workspace ${SOCKEYE_DATA_DIR}
-
-cd ./${SOCKEYE_DATA_DIR}/systems/${LANGUAGE_DATA}/data/bpe
+cd ./${DATA_DIR}/systems/${LANGUAGE_DATA}/data/bpe
 gunzip *
 paste -d '|'  train.src train.trg | shuf | awk -v FS='|' '{ print $1 > "train.shuf.src" ; print $2 > "train.shuf.trg" }'
