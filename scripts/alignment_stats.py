22fe52acfdb543cfdd67e6f76ed12fcb551777ca
Markus Göckeritz <markus@goeckeritz.ch>
2019-09-24 16:45:52 +0200

    alignments script

--- scripts/alignment_stats.py
+++ scripts/alignment_stats.py
@@ -1,7 +1,5 @@
 import sys
 
-print(sys.argv)
-
 if len(sys.argv) < 2:
     print("Error: Please add the path to the alignment file as an argument.")
     exit()
@@ -9,16 +7,39 @@ if len(sys.argv) < 2:
 
 def split_fast_align_pair(aStr):
     parts = aStr.split('-')
-    print('parts: ', parts[0], parts[1])
     return (int(parts[0], 10), int(parts[1], 10))
 
+
+def sum(a, b):
+    return a + b
+
 alignments_file = sys.argv[1]
-distance = 0
+pair_distances = []
+line_distances = []
+total_distance = 0
+total_num_words = 0
 with open(alignments_file) as file:
     for line in file:
-        for pair in line.split():
-            print(pair)
-            i, j = split_fast_align_pair(line)
-            distance += abs(i - j)
-            print(line, distance)
-        exit()
+        line_distance = 0
+        words = line.split()
+        for pair in words:
+            i, j = split_fast_align_pair(pair)
+            distance = abs(i - j)
+            line_distance += distance
+            total_distance += distance
+            pair_distances.append(distance)
+        line_distances.append(line_distance)
+        total_num_words += len(words)
+
+average_pair_distance = reduce(sum, pair_distances) / len(pair_distances)
+average_line_distance = reduce(sum, line_distances) / len(line_distances)
+
+print("")
+print("ALIGNMENT STATISTICS")
+print("=========================")
+print("Number of Lines: %d" % len(line_distances))
+print("Number of Words: %d" % total_num_words)
+print("Total Distance: %d" % total_distance)
+print("Average Pair Distance: %d" % average_pair_distance)
+print("Average Line Distance: %d" % average_line_distance)
+print("")
