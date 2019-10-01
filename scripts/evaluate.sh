5c84f6d5334840bc071489987f4002d3154cbcfd
Markus Göckeritz <markus@markuss-mbp.home>
2019-10-01 21:56:48 +0200

    added beam size to evaluate

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -7,7 +7,7 @@ MODEL="exps/model390000.pt"
 DATA="corpus/WMTENDE/4pad"
 SRC_PATH="sockeye_autopilot/systems/wmt14_en_de/data/bpe/test.0.src"
 #SRC_PATH="sockeye_autopilot/systems/wmt14_en_de/data/bpe/dev.src"
-BEAM_SIZE=5
+BEAM_SIZES=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35)
 TARGET_TRANSLATION="sockeye_autopilot/systems/wmt14_en_de/data/tst/test.0.trg"
 #TARGET_TRANSLATION="sockeye_autopilot/systems/wmt14_en_de/data/tst/dev.trg"
 EPSILON_LIMIT=3
@@ -15,21 +15,31 @@ SRC_EPSILON_INJECTION=4
 START_PADS=(0 1 2 3 4 5)
 LANGUAGE="de"
 SAVE_DIR="output/"
+FILE_NAME="translation_test.txt"
 
-for START_PAD in "${START_PADS[@]}"
+name=$(date +"%m-%d-%y")
+
+
+for BEAM_SIZE in "${BEAM_SIZES[@]}"
 do
-    mkdir -p ${SAVE_DIR}${START_PAD}
-    python model/generate.py \
-          --checkpoint ${MODEL} \
-          --data ${DATA} \
-          --src_path ${SRC_PATH} \
-          --beam_size ${BEAM_SIZE} \
-          --eval \
-          --target_translation ${TARGET_TRANSLATION} \
-          --epsilon_limit ${EPSILON_LIMIT} \
-          --src_epsilon_injection ${SRC_EPSILON_INJECTION} \
-          --start_pads ${START_PAD} \
-          --language ${LANGUAGE} \
-          --save_dir ${SAVE_DIR}${START_PAD}
-    echo "Generated output for START_PADS=${START_PAD}"
+    for START_PAD in "${START_PADS[@]}"
+        do
+            file_name="${name}_beam_${BEAM_SIZE}_pads_${START_PAD}.txt"
+            mkdir -p ${SAVE_DIR}
+            python model/generate.py \
+                  --checkpoint ${MODEL} \
+                  --data ${DATA} \
+                  --src_path ${SRC_PATH} \
+                  --beam_size ${BEAM_SIZE} \
+                  --eval \
+                  --target_translation ${TARGET_TRANSLATION} \
+                  --epsilon_limit ${EPSILON_LIMIT} \
+                  --src_epsilon_injection ${SRC_EPSILON_INJECTION} \
+                  --start_pads ${START_PAD} \
+                  --language ${LANGUAGE} \
+                  --save_dir ${SAVE_DIR}${START_PAD} \
+                  --file_name ${file_name}
+            echo "Generated output for START_PADS=${START_PAD} and BEAM_SIZE=${BEAM_SIZE}"
+        done
 done
+
