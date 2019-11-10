255a1b71409ecde7cdc7dcd471dffe75a084b7d0
Markus Göckeritz <markus@goeckeritz.ch>
2019-11-10 17:51:56 +0100

    testing json

--- scripts/bleu_to_csv.py
+++ scripts/bleu_to_csv.py
@@ -1,9 +1,11 @@
 import os
 import sys
+import json
 
 directory = sys.argv[1]
 
 for file in os.listdir(directory):
     if file.endswith(".bleu"):
-        with open(os.path.join(directory, file)) as bleu_file:
-            print(bleu_file.readlines())
+        with open(os.path.join(directory, file), "r") as bleu_file:
+            data = json.loads(bleu_file.readlines())
+            print(data["score"])
