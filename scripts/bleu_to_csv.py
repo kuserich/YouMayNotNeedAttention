26f904dc224cf05f8380172a0c74e4524052d3a2
Markus Göckeritz <markus@goeckeritz.ch>
2019-11-10 17:50:43 +0100

    testing readline

--- scripts/bleu_to_csv.py
+++ scripts/bleu_to_csv.py
@@ -5,4 +5,5 @@ directory = sys.argv[1]
 
 for file in os.listdir(directory):
     if file.endswith(".bleu"):
-        print(os.path.join(directory, file))
+        with open(os.path.join(directory, file)) as bleu_file:
+            print(bleu_file.readlines())
