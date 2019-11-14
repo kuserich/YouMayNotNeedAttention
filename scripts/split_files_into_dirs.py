e24bd29e66a548ddc939428700de3764af38ef91
Markus Göckeritz <markus@goeckeritz.ch>
2019-11-14 22:55:07 +0100

    added split script

--- scripts/split_files_into_dirs.py
+++ scripts/split_files_into_dirs.py
@@ -1,11 +1,30 @@
 import sys
 import os
+import math
 
 if len(sys.argv) < 3:
     print("Error: Please specify directory and number of files per directory")
 
 src_dir = sys.argv[1]
-num_dirs = sys.argv[2]
+num_dirs = int(sys.argv[2])
 
+num_files = len([file for file in os.listdir(src_dir)])
+num_files_per_dir = math.floor(num_files / num_dirs)
+
+print("INFO: There are %s files in '%s'" % (num_files, src_dir))
+print("INFO: %s directories will be created with %d files each" % (num_dirs, num_files_per_dir))
+
+
+dir_cnt = 0
+file_cnt = 0
 for file in sorted(os.listdir(src_dir)):
-    print(file)
+    if file_cnt % num_files_per_dir == 0:
+        file_cnt = 0
+        dir_cnt += 1
+
+        if not os.path.exists(os.path.join(src_dir, str(dir_cnt))):
+            os.makedirs(os.path.join(src_dir, str(dir_cnt)))
+
+    file_cnt += 1
+
+    os.move(os.path.join(os.path.join(src_dir, str(dir_cnt)), file))
