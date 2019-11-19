812be91c225b553f976f5014e514490721e6342e
Markus Göckeritz <markus@goeckeritz.ch>
2019-11-19 11:30:10 +0100

    changed src path dir

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -20,7 +20,7 @@ DATA="data/10-25-align-eps/corpus"
 #MODEL="data/wmt14_en_de/bpe/exps/20191031-170132/model13000.pt"
 #SRC_PATH="sockeye_autopilot/systems/wmt14_en_de/data/bpe/test.0.src"
 #SRC_PATH="sockeye_autopilot/systems/wmt14_en_de/data/bpe/dev.src"
-SRC_PATH="sockeye_autopilot/systems/wmt14_en_de/data/bpe-split/27/x990332"
+SRC_PATH="sockeye_autopilot/systems/wmt14_en_de/data/bpe-split/27/x990331"
 #BEAM_SIZES=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35)
 BEAM_SIZES=(5)
 #TARGET_TRANSLATION="sockeye_autopilot/systems/wmt14_en_de/data/tst/test.0.trg"
@@ -32,7 +32,7 @@ START_PADS=(4)
 LANGUAGE="de"
 SAVE_DIR="output/training/"
 FILE_NAME="translation_test.txt"
-PREFIX="x990332_"
+PREFIX="x990331_"
 SUFFIX=""
 
 if [[ -d $SRC_PATH ]]; then
