318ade64ebc757852b76fe9b840b48c21deeaef5
Markus Göckeritz <markus@goeckeritz.ch>
2019-11-22 20:05:51 +0100

    added continue train back

--- model/main.py
+++ model/main.py
@@ -63,8 +63,8 @@ parser.add_argument('--save', type=str, default='exp',
 
 parser.add_argument('--wdecay', type=float, default=1.2e-6,
                     help='weight decay applied to all weights')
-# parser.add_argument('--continue_train', action='store_true',
-#                     help='continue train from a checkpoint')
+parser.add_argument('--continue_train', action='store_true',
+                    help='continue train from a checkpoint')
 
 args = parser.parse_args()
 
@@ -112,11 +112,11 @@ test_data_src, test_data_trg = batchify(corpus.valid_src, corpus.valid_trg, test
 
 ntokens = len(corpus.dictionary)
 
-# if args.continue_train:  
-#     model = torch.load(os.path.join(args.save, 'model.pt'))
-#     print("Loaded existing model.")
-# else:
-model = model.RNNModel('LSTM', ntokens, args.emsize, args.nhid, args.nlayers, args.dropout, args.dropouth,
+if args.continue_train:
+    model = torch.load(os.path.join(args.save, 'model.pt'))
+    print("Loaded existing model.")
+else:
+    model = model.RNNModel('LSTM', ntokens, args.emsize, args.nhid, args.nlayers, args.dropout, args.dropouth,
                        args.dropouti, args.dropoute, args.wdrop, args.dropoutcomb, args.tied)
 weight = torch.ones(len(corpus.dictionary))
 epsilon = corpus.dictionary.word2idx["@@@"]
