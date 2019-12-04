02f1ad298d385d010935987710d83ae20ff9488d
Markus Göckeritz <markus@goeckeritz.ch>
2019-12-04 16:38:37 +0100

    changed beam and gpu

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -3,7 +3,7 @@ BASE_DIR=${SCRIPTS_DIR}/..
 
 source ${SCRIPTS_DIR}/config.sh
 
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=1
 
 
 # Model and corpus of child model
@@ -32,7 +32,7 @@ SRC_PATH="sockeye_autopilot/systems/wmt14_en_de/data/bpe/dev.src"
 #SRC_PATH="/mnt/storage/scratch1/goeckeritz/dev.en"
 #SRC_PATH="sockeye_autopilot/systems/wmt14_en_de/data/bpe-split-all/x990230"
 #BEAM_SIZES=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35)
-BEAM_SIZES=(16 17 18 19 20 21 22 23 24 25 26 27 28 29 30)
+BEAM_SIZES=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15)
 #TARGET_TRANSLATION="sockeye_autopilot/systems/wmt14_en_de/data/tst/test.0.trg"
 TARGET_TRANSLATION="sockeye_autopilot/systems/wmt14_en_de/data/tst/dev.trg"
 EPSILON_LIMITS=(3)
